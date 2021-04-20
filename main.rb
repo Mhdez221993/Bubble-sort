f61e63a8ec982eb8f4781bb3c2947960acbeb445
Moises <mhdezcoronado@gmail.com>
2021-04-20 15:03:39 +0200

    Fix linter error

--- main.rb
+++ main.rb
@@ -1,32 +1,26 @@
-def buble_sort (array)
-    n = array.length-1
-    loop do
-        swapped = false
-        for i in 1..n
-            if array[i-1].to_i > array[i].to_i
-                array[i-1], array[i] = array[i], array[i-1]
-                swapped = true
-            end
-        end          
-        if swapped == false
-            break
-        end
-   end
-   array
+# frozen_string_literal: true
+
+def buble_sort(array)
+  n = array.length - 1
+  loop do
+    swapped = false
+    (1..n).each do |i|
+      array[i - 1], array[i] = array[i], array[i - 1], swapped = true if array[i - 1].to_i > array[i].to_i
+    end
+    break if swapped == false
+  end
+  array
 end
 
-def buble_sort_by (array)
-    n = array.length-1
-  
-    loop do
-        swapped = false
-        for i in 1..n
-            sort_it = yield(array[i-1], array[i])
-            array[i-1], array[i] = array[i], array[i-1], swapped = true if   sort_it>0
-        end          
-        break if swapped == false
-   end
-   array
+def buble_sort_by(array)
+  loop do
+    swapped = false
+    (1..array.length - 1).each do |i|
+      sort_it = yield(array[i - 1], array[i])
+      array[i - 1], array[i] = array[i], array[i - 1], swapped = true if sort_it.positive?
+    end
+    break if swapped == false
+  end
+  array
 end
-
-p buble_sort_by (["hi","hello","hey", 'free style', 'h', 'free', '']) {|left, right| left.length - right.length}
+p buble_sort_by(['hi', 'hello', 'hey', 'free style', 'h', 'free', '']) { |left, right| left.length - right.length }
