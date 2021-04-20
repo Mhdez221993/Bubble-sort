731d53e3ab5153769e566d0d6de976cf82af7921
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-20 11:07:27 +0200

    Add code to main.rb file

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
-def buble_sort do
-
+def buble_sort (array) 
+    n = array.length-2
+    swapped = true
+    while swapped == true do
+        swapped = false
+        arr = array
+        i=0
+        while i< n do 
+            if arr[i] > arr[i+1] 
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = true
+            end
+        end
+    end
 end
+
+p buble_sort([2,5,3,6,4])
